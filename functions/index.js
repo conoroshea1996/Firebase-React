786e682ba449e7baf9369d6d67637b0edacd3a7b
conoroshea1996 <conoshea12@gmail.com>
2019-09-09 15:11:59 +0100

    first commit

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello World!");
+});
+
+exports.getPosts = functions.https.onRequest((request, response) => {
+    admin.firestore().collection('posts').get()
+        .then((data) => {
+            let posts = [];
+            data.forEach(doc => {
+                posts.push(doc.data());
+            });
+            return response.json(posts)
+        })
+        .catch(err => console.log(err))
+})
+
+exports.createPosts = functions.https.onRequest((request, response) => {
+
+})
