b2920b7e2f7d9a7244ec9a361cafd84da4884271
conoroshea1996 <conoshea12@gmail.com>
2019-09-12 23:22:17 +0100

    add login route and authenticate using firbase

--- functions/index.js
+++ functions/index.js
@@ -159,8 +159,40 @@ app.post('/signup', (request, response) => {
                 return response.status(500).json({ error: err.code });
             }
         })
-
 })
+
+// Login Route
+app.post('/login', (request, response) => {
+    const user = {
+        email: request.body.email,
+        password: request.body.password
+    }
+
+    let errors = {};
+
+    if (isEmpty(user.email)) {
+        errors.email = 'Must not be empty';
+    }
+    if (isEmpty(user.password)) {
+        errors.password = 'Must not be empty';
+    }
+
+    if (Object.keys(errors).length > 0) {
+        return response.status(400).json(errors)
+    }
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        }).then(token => {
+            return response.json({ token, login: true });
+        }).catch(err => {
+            return response.status(500).json({ error: err.code });
+        })
+})
+
+
+
 
 
 exports.api = functions.region('europe-west1').https.onRequest(app)
